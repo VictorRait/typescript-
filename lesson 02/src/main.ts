04f991d807de1380e418ac78e46a1ad48a75c207
VictorRait <victorlemuelrait@gmail.com>
2023-11-30 09:05:52 +0800

    lesson 2 complete

--- lesson 02/src/main.ts
+++ lesson 02/src/main.ts
@@ -1,3 +1,18 @@
 let myName: string;
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
 
 myName = "dave";
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+
+const sum = (a: number, b: string) => {
+	return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
