f5b064932c143649f54c1faf813755b096d3cf13
VictorRait <victorlemuelrait@gmail.com>
2023-12-18 02:42:31 +0800

    completed lesson 9

---
+++ lesson 09/build/js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+// utility types
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
+};
+const assign1 = {
+    studentId: "compsci123",
+    title: "Final Project",
+    grade: 0,
+};
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded = updateAssignment(assign1, { grade: 95 });
+// Required and Readonly
+const recordAssignment = (assign) => {
+    // send to database, etc.
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+// Record
+const hexColorMap = {
+    red: "FF000",
+    green: "00FF00",
+    blue: "0000FF",
+};
+const finalGrades = {
+    Sara: "B",
+    Kelly: "U",
+};
+const gradeData = {
+    Sara: { assign1: 85, assign2: 93 },
+    Kelly: { assign1: 76, assign2: 15 },
+};
+const score = {
+    studentId: "k123",
+    grade: 85,
+};
+const preview = {
+    studentId: "k123",
+    title: "Final Project",
+};
+const createNewAssign = (title, points) => {
+    return { title, points };
+};
